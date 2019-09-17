8bbcb8664709fc4a734f8f2a1d35bc91b674e3c8
DenislavNedev <dnedev98@gmail.com>
2019-09-17 14:57:49 +0300

    Adapter Design Pattern

---
+++ src/com/design/patterns/creational/structural/adapter/AdvancedMediaPlayer.java
@@ -0,0 +1,7 @@
+package com.design.patterns.creational.structural.adapter;
+
+public interface AdvancedMediaPlayer {
+    public void playVlc(String fileName);
+
+    public void playMp4(String fileName);
+}
